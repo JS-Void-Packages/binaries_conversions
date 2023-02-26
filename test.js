a58a15e0c5f5723dc3fc0d80fe0212032931e706
Mrthomas20121 <thomas77300@gmail.com>
2023-02-26 12:51:08 +0100

    Tests

---
+++ test.js
@@ -0,0 +1,18 @@
+const { HexToBinary, BinaryToDecimal, OctalToHex, DecimalToOctal } = require('./index');
+
+let n1 = '4F';
+let n2 = '0111';
+let n3 = '882';
+let n4 = 50;
+
+// convert hex to binary
+console.log(HexToBinary(n1));
+
+// convert binary to decimal
+console.log(BinaryToDecimal(n2));
+
+// convert octal to hex
+console.log(OctalToHex(n3));
+
+// convert decimal to octal
+console.log(DecimalToOctal(n4));
